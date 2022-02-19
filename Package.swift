96d6059c67dc7e51a0bf75d62b3c326e60f543f8
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-02-19 09:08:23 -0500

    Explicit os minimums

--- Package.swift
+++ Package.swift
@@ -1,18 +1,17 @@
 // swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "LanguageServerProtocol",
+    platforms: [.macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "LanguageServerProtocol",
             targets: ["LanguageServerProtocol"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ChimeHQ/JSONRPC", from: "0.3.0"),
+        .package(url: "https://github.com/ChimeHQ/JSONRPC", from: "0.3.1"),
     ],
     targets: [
         .target(
