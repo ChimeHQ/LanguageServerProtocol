b42518e9d57dbf28952248bc8385fd3d5954e98b
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-03-25 08:15:01 -0400

    Optional snuck in there

--- Sources/LanguageServerProtocol/LanguageFeatures/ColorPresentation.swift
+++ Sources/LanguageServerProtocol/LanguageFeatures/ColorPresentation.swift
@@ -3,11 +3,11 @@ import Foundation
 public struct ColorPresentationParams: Codable, Hashable {
     public let workDoneToken: ProgressToken?
     public let partialResultToken: ProgressToken?
-    public let textDocument: TextDocumentIdentifier?
+    public let textDocument: TextDocumentIdentifier
     public let color: Color
     public let range: LSPRange
 
-    public init(workDoneToken: ProgressToken? = nil, partialResultToken: ProgressToken? = nil, textDocument: TextDocumentIdentifier?, color: Color, range: LSPRange) {
+    public init(workDoneToken: ProgressToken? = nil, partialResultToken: ProgressToken? = nil, textDocument: TextDocumentIdentifier, color: Color, range: LSPRange) {
         self.workDoneToken = workDoneToken
         self.partialResultToken = partialResultToken
         self.textDocument = textDocument
