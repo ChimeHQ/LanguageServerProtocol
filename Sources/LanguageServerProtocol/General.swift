abf80b752f5db0aa3f293b30f2ffeebc0f7decdf
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-11-03 18:27:49 -0400

    Locale, clientInfo, and corrected case for rootUri

--- Sources/LanguageServerProtocol/General.swift
+++ Sources/LanguageServerProtocol/General.swift
@@ -8,18 +8,40 @@ public enum Tracing: String, Codable, Hashable {
 }
 
 public struct InitializeParams: Codable {
-    public let processId: Int
+	public struct ClientInfo: Codable, Hashable, Sendable {
+		public let name: String
+		public let version: String?
+
+		public init(name: String, version: String? = nil) {
+			self.name = name
+			self.version = version
+		}
+	}
+
+    public let processId: Int?
+	public let clientInfo: ClientInfo?
+	public let locale: String?
     public let rootPath: String?
-    public let rootURI: DocumentUri?
+    public let rootUri: DocumentUri?
     public let initializationOptions: AnyCodable?
     public let capabilities: ClientCapabilities
     public let trace: Tracing?
     public let workspaceFolders: [WorkspaceFolder]?
 
-    public init(processId: Int, rootPath: String?, rootURI: DocumentUri?, initializationOptions: AnyCodable?, capabilities: ClientCapabilities, trace: Tracing?, workspaceFolders: [WorkspaceFolder]?) {
+    public init(processId: Int,
+				clientInfo: ClientInfo? = nil,
+				locale: String?,
+				rootPath: String?,
+				rootUri: DocumentUri?,
+				initializationOptions: AnyCodable?,
+				capabilities: ClientCapabilities,
+				trace: Tracing?,
+				workspaceFolders: [WorkspaceFolder]?) {
         self.processId = processId
+		self.clientInfo = clientInfo
+		self.locale = locale
         self.rootPath = rootPath
-        self.rootURI = rootURI
+        self.rootUri = rootUri
         self.initializationOptions = initializationOptions
         self.capabilities = capabilities
         self.trace = trace
