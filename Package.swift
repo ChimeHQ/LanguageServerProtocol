cae01173f45b0518ae5533b083edd153633af984
Matt <85322+mattmassicotte@users.noreply.github.com>
2023-06-14 15:57:31 -0400

    Strict JSONRPC package version to prep for big changes

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["LanguageServerProtocol"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ChimeHQ/JSONRPC", from: "0.7.0"),
+		.package(url: "https://github.com/ChimeHQ/JSONRPC", "0.7.0"..."0.7.1"),
     ],
     targets: [
         .target(
