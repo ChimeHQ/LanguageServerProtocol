5563623dce986bbbbf5c15b8b2d6f0870dd534e5
Nils Hjelte <nils.hjelte@gmail.com>
2023-09-27 19:52:51 +0200

    Add missing public constructors

--- Sources/LanguageServerProtocol/LanguageFeatures/Diagnostics.swift
+++ Sources/LanguageServerProtocol/LanguageFeatures/Diagnostics.swift
@@ -5,6 +5,13 @@ public struct DiagnosticOptions: Codable, Hashable, Sendable {
 	public let identifier: String?
 	public let interFileDependencies: Bool
 	public let workspaceDiagnostics: Bool
+
+	public init(workDoneProgress: Bool? = nil, identifier: String? = nil, interFileDependencies: Bool, workspaceDiagnostics: Bool) {
+		self.workDoneProgress = workDoneProgress
+		self.identifier = identifier
+		self.interFileDependencies = interFileDependencies
+		self.workspaceDiagnostics = workspaceDiagnostics
+	}
 }
 
 public struct DiagnosticRegistrationOptions: Codable, Hashable, Sendable {
@@ -14,6 +21,15 @@ public struct DiagnosticRegistrationOptions: Codable, Hashable, Sendable {
 	public let interFileDependencies: Bool
 	public let workspaceDiagnostics: Bool
 	public let id: String?
+
+	public init(documentSelector: DocumentSelector? = nil, workDoneProgress: Bool? = nil, identifier: String? = nil, interFileDependencies: Bool, workspaceDiagnostics: Bool, id: String? = nil) {
+		self.documentSelector = documentSelector
+		self.workDoneProgress = workDoneProgress
+		self.identifier = identifier
+		self.interFileDependencies = interFileDependencies
+		self.workspaceDiagnostics = workspaceDiagnostics
+		self.id = id
+	}
 }
 
 public struct PublishDiagnosticsClientCapabilities: Codable, Hashable, Sendable {
@@ -45,6 +61,11 @@ public struct DiagnosticClientCapabilities: Codable, Hashable, Sendable {
 public struct DiagnosticRelatedInformation: Codable, Hashable, Sendable {
     public let location: Location
     public let message: String
+
+	public init(location: Location, message: String) {
+		self.location = location
+		self.message = message
+	}
 }
 
 public typealias DiagnosticCode = TwoTypeOption<Int, String>
@@ -61,20 +82,40 @@ public enum DiagnosticTag: Int, CaseIterable, Codable, Hashable, Sendable {
     case deprecated = 2
 }
 
+public typealias CodeDescription = String
+
 public struct Diagnostic: Codable, Hashable, Sendable {
     public let range: LSPRange
     public let severity: DiagnosticSeverity?
     public let code: DiagnosticCode?
+	public let codeDescription: CodeDescription?
     public let source: String?
     public let message: String
     public let tags: [DiagnosticTag]?
     public let relatedInformation: [DiagnosticRelatedInformation]?
+
+    public init(range: LSPRange, severity: DiagnosticSeverity? = nil, code: DiagnosticCode? = nil, codeDescription: CodeDescription? = nil, source: String? = nil, message: String, tags: [DiagnosticTag]? = nil, relatedInformation: [DiagnosticRelatedInformation]? = nil) {
+      self.range = range
+      self.severity = severity
+      self.code = code
+      self.codeDescription = codeDescription
+      self.source = source
+      self.message = message
+      self.tags = tags
+      self.relatedInformation = relatedInformation
+  }
 }
 
 public struct PublishDiagnosticsParams: Codable, Hashable, Sendable {
     public let uri: DocumentUri
     public let version: Int?
     public let diagnostics: [Diagnostic]
+
+	public init(uri: DocumentUri, version: Int? = nil, diagnostics: [Diagnostic]) {
+		self.uri = uri
+		self.version = version
+		self.diagnostics = diagnostics
+	}
 }
 
 public struct DocumentDiagnosticParams: Codable, Hashable, Sendable {
@@ -108,6 +149,12 @@ public struct BaseDocumentDiagnosticReport: Codable, Hashable, Sendable {
 	public let kind: DocumentDiagnosticReportKind
 	public let resultId: String?
 	public let items: [Diagnostic]?
+
+	public init(kind: DocumentDiagnosticReportKind, resultId: String? = nil, items: [Diagnostic]? = nil) {
+		self.kind = kind
+		self.resultId = resultId
+		self.items = items
+	}
 }
 
 public struct RelatedDocumentDiagnosticReport: Codable, Hashable, Sendable {
@@ -115,6 +162,13 @@ public struct RelatedDocumentDiagnosticReport: Codable, Hashable, Sendable {
 	public let resultId: String?
 	public let items: [Diagnostic]?
 	public let relatedDocuments: [DocumentUri: DocumentDiagnosticReport]?
+
+	public init(kind: DocumentDiagnosticReportKind, resultId: String? = nil, items: [Diagnostic]? = nil, relatedDocuments: [DocumentUri : DocumentDiagnosticReport]? = nil) {
+		self.kind = kind
+		self.resultId = resultId
+		self.items = items
+		self.relatedDocuments = relatedDocuments
+	}
 }
 
 typealias FullDocumentDiagnosticReport = BaseDocumentDiagnosticReport
