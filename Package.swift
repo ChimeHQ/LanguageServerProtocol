1191a6d7bb1f73edf2b9b774a4bd21999a52221d
Matt <85322+mattmassicotte@users.noreply.github.com>
2023-10-19 15:16:02 -0400

    Keep JSONRPC < 0.9.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version: 5.8
 
 import PackageDescription
 
@@ -7,24 +7,24 @@ let settings: [SwiftSetting] = [
 ]
 
 let package = Package(
-    name: "LanguageServerProtocol",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
-    products: [
-        .library(
-            name: "LanguageServerProtocol",
-            targets: ["LanguageServerProtocol"]),
-    ],
-    dependencies: [
-		.package(url: "https://github.com/ChimeHQ/JSONRPC", from: "0.8.0"),
-    ],
-    targets: [
-        .target(
-            name: "LanguageServerProtocol",
-            dependencies: ["JSONRPC"],
+	name: "LanguageServerProtocol",
+	platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
+	products: [
+		.library(
+			name: "LanguageServerProtocol",
+			targets: ["LanguageServerProtocol"]),
+	],
+	dependencies: [
+		.package(url: "https://github.com/ChimeHQ/JSONRPC", "0.8.0"..<"0.9.0"),
+	],
+	targets: [
+		.target(
+			name: "LanguageServerProtocol",
+			dependencies: ["JSONRPC"],
 			swiftSettings: settings),
-        .testTarget(
-            name: "LanguageServerProtocolTests",
-            dependencies: ["LanguageServerProtocol"],
+		.testTarget(
+			name: "LanguageServerProtocolTests",
+			dependencies: ["LanguageServerProtocol"],
 			swiftSettings: settings),
-    ]
+	]
 )
