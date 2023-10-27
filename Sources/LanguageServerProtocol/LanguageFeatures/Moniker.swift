c85bae0a6645b39c3fec9e63ae9af21be92bde04
Nils Hjelte <nils.hjelte@gmail.com>
2023-10-27 16:27:34 +0200

    Remove unused code

--- Sources/LanguageServerProtocol/LanguageFeatures/Moniker.swift
+++ Sources/LanguageServerProtocol/LanguageFeatures/Moniker.swift
@@ -23,20 +23,12 @@ public enum UniquenessLevel: Codable, Sendable {
 	case group
 	case scheme
 	case global
-	// public static let document = "document"
-	// public static let project = "project"
-	// public static let group = "group"
-	// public static let scheme = "scheme"
-	// public static let global = "global"
 }
 
 public enum MonikerKind : Codable, Sendable{
 	case _import
 	case export
 	case local
-	// public static let _import = "import"
-	// public static let export = "export"
-	// public static let local = "local"
 }
 
 public struct Moniker : Codable, Sendable {
