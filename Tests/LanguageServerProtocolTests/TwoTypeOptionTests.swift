ed1d88336df305fa9f832198bb683c6f40bedda7
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-04-06 07:21:59 -0400

    Remove JSONRPC from TwoTypeOptionTests

--- Tests/LanguageServerProtocolTests/TwoTypeOptionTests.swift
+++ Tests/LanguageServerProtocolTests/TwoTypeOptionTests.swift
@@ -1,15 +1,15 @@
 import XCTest
 import LanguageServerProtocol
-import JSONRPC
 
-class TwoTypeOptionTests: XCTestCase {
-    func testDecodingKeyMissing() throws {
-        let json = """
-{"jsonrpc":"2.0","result":[{"title":"Organize Imports","kind":"source.organizeImports","edit":{"documentChanges":[{"textDocument":{"version":0,"uri":"file:///.go"},"edits":[{"range":{"start":{"line":4,"character":0},"end":{"line":5,"character":0}},"newText":""}]}]}}],"id":2}
+final class TwoTypeOptionTests: XCTestCase {
+	func testDecodingKeyMissing() throws {
+		// this is a CodeAction, but being very dumb, I did not fully capture what I mean by "missing key"
+		let json = """
+[{"title":"Organize Imports","kind":"source.organizeImports","edit":{"documentChanges":[{"textDocument":{"version":0,"uri":"file:///.go"},"edits":[{"range":{"start":{"line":4,"character":0},"end":{"line":5,"character":0}},"newText":""}]}]}}]
 """
-        let data = try XCTUnwrap(json.data(using: .utf8))
-        let option = try JSONDecoder().decode(JSONRPCResponse<CodeActionResponse>.self, from: data)
+		let data = try XCTUnwrap(json.data(using: .utf8))
+		let option = try JSONDecoder().decode(CodeActionResponse.self, from: data)
 
-        XCTAssertNotNil(option)
-    }
+		XCTAssertNotNil(option)
+	}
 }
